296894bc241a972341797ca6be76ec2f4b16ba56
Sönke Brightside <a@42f.in>
2021-01-13 22:33:09 +0100

    Separate waveshare2in13v2 driver (#2)
    Adds support for EPD2in13v2

---
+++ waveshare2in13v2/doc.go
@@ -0,0 +1,15 @@
+// Copyright 2021 The Periph Authors. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+// Package waveshare2in13v2 controls Waveshare 2in13v2 e-paper display.
+//
+// Datasheets
+//
+// https://www.waveshare.com/w/upload/d/d5/2.13inch_e-Paper_Specification.pdf
+//
+// Product page:
+//
+// 2.13 Inch version 2: https://www.waveshare.com/wiki/2.13inch_e-Paper_HAT
+//
+package waveshare2in13v2
