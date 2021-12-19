3f0429779230ad208c21f0527fee763b9896369d
Michael Hanselmann <public@hansmi.ch>
2021-12-19 07:49:31 +0100

    waveshare2in13v2: Amend documentation with note on GDEH0213B73

--- waveshare2in13v2/doc.go
+++ waveshare2in13v2/doc.go
@@ -2,14 +2,14 @@
 // Use of this source code is governed under the Apache License, Version 2.0
 // that can be found in the LICENSE file.
 
-// Package waveshare2in13v2 controls Waveshare 2in13v2 e-paper display.
+// Package waveshare2in13v2 controls Waveshare 2.13 v2 e-paper displays.
 //
-// Datasheets
-//
+// Datasheet:
 // https://www.waveshare.com/w/upload/d/d5/2.13inch_e-Paper_Specification.pdf
 //
 // Product page:
+// 2.13 inch version 2: https://www.waveshare.com/wiki/2.13inch_e-Paper_HAT
 //
-// 2.13 Inch version 2: https://www.waveshare.com/wiki/2.13inch_e-Paper_HAT
-//
+// The Waveshare 2.13in v2 display is compatible with the GoodDisplay
+// GDEH0213B73.
 package waveshare2in13v2
